16a71dda909c35d4d17866a13d6ca15c2dc387c9
OKAUEND <def1f23sbb@gmail.com>
2020-03-05 00:03:03 +0900

    ADD - Firestoreからデータを追加、取得できるように また、試験的に複数ドキュメントを取得できるように試作 画像をStorgaeから取得し、imgへバインドできるかをテスト

--- src/components/Console/LogRegister.vue
+++ src/components/Console/LogRegister.vue
@@ -3,47 +3,67 @@
     <div v-if="isUpadateMode">更新モード</div>
     <div v-else-if="!isUpadateMode">新規追加モード</div>
     <button @click="isUpadateMode = false">新規追加モードへ</button>
-    <div>
+    <div class="LogRegister__Body">
       <div>名前:<input type="text" v-model="CraftingLog.name" /></div>
       <div>画像パス:<input type="text" v-model="CraftingLog.image" /></div>
+      <div>
+        下限レベル:<input type="text" v-model="CraftingLog.bottomLevel" />
+      </div>
+      <div>
+        上限レベル:<input type="text" v-model="CraftingLog.upperLevel" />
+      </div>
+      <div>
+        アイテムレベル:<input type="text" v-model="CraftingLog.ItemLevel" />
+      </div>
       <div class="LogRegister__Ingredients">
         <ingredient
           :storeLog="CraftingLog.Ingredients.slot0"
+          :SortID="1"
           @change="setSlot0"
         />
         <ingredient
           :storeLog="CraftingLog.Ingredients.slot1"
+          :SortID="2"
           @change="setSlot1"
         />
         <ingredient
           :storeLog="CraftingLog.Ingredients.slot2"
+          :SortID="3"
           @change="setSlot2"
         />
         <ingredient
           :storeLog="CraftingLog.Ingredients.slot3"
+          :SortID="4"
           @change="setSlot3"
         />
         <ingredient
           :storeLog="CraftingLog.Ingredients.slot4"
+          :SortID="5"
           @change="setSlot4"
         />
         <ingredient
           :storeLog="CraftingLog.Ingredients.slot5"
+          :SortID="6"
           @change="setSlot5"
         />
       </div>
     </div>
     <button @click="writeInterface()">追加/更新</button>
+    <input type="text" placeholder="下限レベル" />
+    <input type="text" placeholder="上限レベル" />
+    <button @click="fetchCraftLogs()">取得</button>
     <ul>
       <li v-for="(Log, ID) in Logs" :key="ID">
         <button @click="bindLog(Log)">{{ Log.name }}</button>
       </li>
     </ul>
+    <img :src="iconpath" />
+    <button @click="fetchIconsList()">アイコン取得</button>
   </div>
 </template>
 
 <script>
-// import firebase from "@/firebase.js";
+import firebase from "@/firebase.js";
 import Ingredient from "@/components/Console/Ingredient.vue";
 export default {
   name: "LogRegister",
@@ -55,6 +75,9 @@ export default {
       CraftingLog: {
         name: "",
         image: "",
+        bottomLevel: 0,
+        upperLevel: 0,
+        ItemLevel: 0,
         Ingredients: {
           slot0: {},
           slot1: {},
@@ -66,14 +89,15 @@ export default {
       },
       name: "TEST",
       Icon: "TEST",
-      slot0: { isEnable: false },
-      slot1: { isEnable: false },
-      slot2: { isEnable: false },
-      slot3: { isEnable: false },
-      slot4: { isEnable: false },
-      slot5: { isEnable: false },
+      slot0: { isEnable: false, IngredientID: 0 },
+      slot1: { isEnable: false, IngredientID: 0 },
+      slot2: { isEnable: false, IngredientID: 0 },
+      slot3: { isEnable: false, IngredientID: 0 },
+      slot4: { isEnable: false, IngredientID: 0 },
+      slot5: { isEnable: false, IngredientID: 0 },
       Logs: [],
-      isUpadateMode: false
+      isUpadateMode: true,
+      iconpath: ""
     };
   },
   computed: {
@@ -144,160 +168,209 @@ export default {
     },
     setSlot5(val) {
       this.IngredientSlot5 = val;
+    },
+    async fetchCraftLogs() {
+      const docRef = firebase
+        .firestore()
+        .collection("CraftLog")
+        .orderBy("ID");
+      const storeLogs = await docRef
+        .get()
+        .then(querySnapshot => {
+          const docs = querySnapshot.docs.map(doc => doc.data());
+          alert("First Getting Document");
+          return docs;
+        })
+        .catch(error => {
+          console.error("Firest Error getting document", error);
+        });
+      this.Logs = storeLogs;
+    },
+    async createCollectionRef(DocumentPath) {
+      return firebase.firestore().doc(DocumentPath);
+    },
+    async createFirestoreDocRef(DocumentPath) {
+      const DocRef = await firebase.firestore().doc(DocumentPath);
+      return await DocRef.get().then(function(doc) {
+        return doc.data();
+      });
+    },
+    async createIngredientsArray(Ingredients) {
+      console.log("Ingredients", Ingredients);
+
+      const Slot0Ingredients = await this.testFunction(
+        Ingredients.slot0.ChildDocument.path
+      );
+
+      const Slot0Data = { ...Slot0Ingredients, value: Ingredients.slot0.value };
+
+      console.log("Slot0Data", Slot0Data);
+
+      console.log("Slot1", Ingredients.slot1);
+      const Slot1Ingredients = await this.testFunction(
+        Ingredients.slot1.ChildDocument.path
+      );
+      console.log("Slot1", Ingredients.slot1.ChildDocument.path);
+      const Slot1Data = { ...Slot1Ingredients, value: Ingredients.slot1.value };
+      console.log("Slot1Data", Slot1Data);
+      return Slot0Data;
+    },
+    async testFunction(IngredientPath) {
+      const LogData = await this.createFirestoreDocRef(IngredientPath).then(
+        value => {
+          return value;
+        }
+      );
+
+      //子の必須素材情報がなかった場合は、処理を抜ける
+      //フラグ管理にしたほうがいいかもしれない
+      if (LogData.Ingredients === undefined) {
+        return {
+          name: LogData.name,
+          image: LogData.image
+        };
+      }
+
+      //再帰的に処理をし、親から見て孫の素材情報を取得する
+      const ChildIngredients = await this.createIngredientsArray(
+        LogData.Ingredients
+      ).then(value => {
+        console.log("Ingredients", value);
+        return value ? value : [];
+      });
+      return {
+        name: LogData.name,
+        image: LogData.image,
+        ingredients: ChildIngredients
+      };
+    },
+    writeInterface() {
+      const Ingredients = {
+        slot0: this.IngredientSlot0,
+        slot1: this.IngredientSlot1,
+        slot2: this.IngredientSlot2,
+        slot3: this.IngredientSlot3,
+        slot4: this.IngredientSlot4,
+        slot5: this.IngredientSlot5
+      };
+
+      if (this.CraftingLog.name === "") {
+        alert("名前が入力されていません");
+        return;
+      }
+
+      const NewLogDataNoID = {
+        name: this.CraftingLog.name,
+        image: this.CraftingLog.image,
+        upperLevel: this.CraftingLog.upperLevel,
+        bottomLevel: this.CraftingLog.bottomLevel,
+        ItemLevel: this.CraftingLog.ItemLevel,
+        Ingredients: Ingredients
+      };
+      console.log(NewLogDataNoID);
+      this.fetchLastID().then(ID => {
+        const nextNumber = this.isUpadateMode ? ID : ID + 1;
+        const Logdata = { ...NewLogDataNoID, ID: nextNumber };
+        const ZeroPadding = `"0000000${nextNumber}`.slice(-7);
+        const DocumentID = `Log${ZeroPadding}`;
+        return this.writeStoreLog(DocumentID, Logdata);
+      });
+      // this.writeStoreLog(NewLogDataNoID);
+    },
+    async writeStoreLog(DocumentID, LogData) {
+      return await firebase
+        .firestore()
+        .collection("CraftLog")
+        .doc(DocumentID)
+        .set(LogData);
+
+      //   .then(function() {
+      //     console.log("Document successfully written!");
+      //     const DocID = firebase
+      //       .firestore()
+      //       .collection("CraftingLog")
+      //       .orderBy("ID");
+      //     return DocID.get();
+      //   })
+      //   .then(function(querySnapshot) {
+      //     const docs = querySnapshot.docs.map(doc => doc.data());
+      //     console.log(docs);
+      //     return docs;
+      //   })
+      //   .catch(function(error) {
+      //     console.error("Error writing document: ", error);
+      //   });
+
+      // console.log("RESULT", Logs);
+      // this.Logs = Logs;
+    },
+    async fetchLastID() {
+      const LogCollection = firebase.firestore().collection("CraftLog");
+      const LastLog = LogCollection.orderBy("ID", "desc").limit(1);
+      return await LastLog.get().then(function(querySnapshot) {
+        return querySnapshot.docs.map(doc => doc.data().ID)[0];
+      });
+    },
+    bindLog(Log) {
+      console.log("BIND", Log);
+      console.log("BIND", Log.name);
+      this.CraftingLog = Log;
+      this.isUpadateMode = true;
+    },
+    fetchIcon() {
+      const storageRef = firebase.storage().ref();
+      storageRef
+        .child("Material/End/Wood1.png")
+        .getDownloadURL()
+        .then(url => {
+          const test = url;
+          console.log("TEST", test);
+          this.iconpath = test;
+        });
+      console.log(this.iconpath);
+    },
+    fetchIconsList() {
+      const ImageRef = firebase
+        .firestore()
+        .collection("Image")
+        .doc("Material")
+        .collection("End")
+        .where("ProductionStageLevel", "==", 1)
+        .where("type", "==", 1);
+      ImageRef.get().then(querySnapshot => {
+        console.log(querySnapshot.docs.map(doc => doc.data().path));
+      });
+      // const storageRef = firebase.storage().ref();
+      // storageRef
+      //   .child("Material/End/Wood1.png")
+      //   .getDownloadURL()
+      //   .then(url => {
+      //     const test = url;
+      //     console.log("TEST", test);
+      //     this.iconpath = test;
+      //   });
+      // console.log(this.iconpath);
     }
-    //   async createdComponent() {
-    //     const docRef = firebase
-    //       .firestore()
-    //       .collection("CraftingLog")
-    //       .orderBy("ID");
-    //     const storeLogs = await docRef
-    //       .get()
-    //       .then(querySnapshot => {
-    //         const docs = querySnapshot.docs.map(doc => doc.data());
-    //         alert("First Getting Document");
-    //         return docs;
-    //       })
-    //       .catch(error => {
-    //         console.error("Firest Error getting document", error);
-    //       });
-    //     this.Logs = storeLogs;
-    //   },
-    //   async createCollectionRef(DocumentPath) {
-    //     return firebase.firestore().doc(DocumentPath);
-    //   },
-    //   async createFirestoreDocRef(DocumentPath) {
-    //     const DocRef = await firebase.firestore().doc(DocumentPath);
-    //     return await DocRef.get().then(function(doc) {
-    //       return doc.data();
-    //     });
-    //   },
-    //   async createIngredientsArray(Ingredients) {
-    //     console.log("Ingredients", Ingredients);
-
-    //     const Slot0Ingredients = await this.testFunction(
-    //       Ingredients.slot0.ChildDocument.path
-    //     );
-
-    //     const Slot0Data = { ...Slot0Ingredients, value: Ingredients.slot0.value };
-
-    //     console.log("Slot0Data", Slot0Data);
-
-    //     console.log("Slot1", Ingredients.slot1);
-    //     const Slot1Ingredients = await this.testFunction(
-    //       Ingredients.slot1.ChildDocument.path
-    //     );
-    //     console.log("Slot1", Ingredients.slot1.ChildDocument.path);
-    //     const Slot1Data = { ...Slot1Ingredients, value: Ingredients.slot1.value };
-    //     console.log("Slot1Data", Slot1Data);
-    //     return Slot0Data;
-    //   },
-    //   async testFunction(IngredientPath) {
-    //     const LogData = await this.createFirestoreDocRef(IngredientPath).then(
-    //       value => {
-    //         return value;
-    //       }
-    //     );
-
-    //     //子の必須素材情報がなかった場合は、処理を抜ける
-    //     //フラグ管理にしたほうがいいかもしれない
-    //     if (LogData.Ingredients === undefined) {
-    //       return {
-    //         name: LogData.name,
-    //         image: LogData.image
-    //       };
-    //     }
-
-    //     //再帰的に処理をし、親から見て孫の素材情報を取得する
-    //     const ChildIngredients = await this.createIngredientsArray(
-    //       LogData.Ingredients
-    //     ).then(value => {
-    //       console.log("Ingredients", value);
-    //       return value ? value : [];
-    //     });
-    //     return {
-    //       name: LogData.name,
-    //       image: LogData.image,
-    //       ingredients: ChildIngredients
-    //     };
-    //   },
-    //   writeInterface() {
-    //     const Ingredients = {
-    //       slot0: this.IngredientSlot0,
-    //       slot1: this.IngredientSlot1,
-    //       slot2: this.IngredientSlot2,
-    //       slot3: this.IngredientSlot3,
-    //       slot4: this.IngredientSlot4,
-    //       slot5: this.IngredientSlot5
-    //     };
-    //     const NewLogDataNoID = {
-    //       name: this.CraftingLog.name,
-    //       image: this.CraftingLog.image,
-    //       Ingredients: Ingredients
-    //     };
-    //     if (this.isUpadate === true) {
-    //       console.log("NEW", "新規作成");
-    //       // this.writeStoreLog(NewLogDataNoID);
-    //     } else if (!this.isUpadate) {
-    //       this.updateStoreLog(NewLogDataNoID);
-    //       console.log("UPDATE", "更新");
-    //     }
-    //   },
-    //   async writeStoreLog(NewLogDataNoID) {
-    //     const LogCollection = firebase.firestore().collection("CraftingLog");
-    //     const LastLog = LogCollection.orderBy("ID", "desc").limit(1);
-    //     const Logs = await LastLog.get()
-    //       .then(function(querySnapshot) {
-    //         const DocID = querySnapshot.docs.map(doc => doc.data().ID)[0];
-    //         const ID = DocID > 0 ? DocID + 1 : 6;
-
-    //         const LogData = { ...NewLogDataNoID, ID: ID };
-    //         return firebase
-    //           .firestore()
-    //           .collection("CraftingLog")
-    //           .doc(String(ID))
-    //           .set(LogData);
-    //       })
-    //       .then(function() {
-    //         console.log("Document successfully written!");
-    //         const DocID = firebase
-    //           .firestore()
-    //           .collection("CraftingLog")
-    //           .orderBy("ID");
-    //         return DocID.get();
-    //       })
-    //       .then(function(querySnapshot) {
-    //         const docs = querySnapshot.docs.map(doc => doc.data());
-    //         console.log(docs);
-    //         return docs;
-    //       })
-    //       .catch(function(error) {
-    //         console.error("Error writing document: ", error);
-    //       });
-
-    //     console.log("RESULT", Logs);
-    //     this.Logs = Logs;
-    //   },
-    //   async updateStoreLog(NewLogDataNoID) {
-    //     console.log("UPDATE", NewLogDataNoID);
-    //   },
-    //   bindLog(Log) {
-    //     console.log("BIND", Log);
-    //     console.log("BIND", Log.name);
-    //     this.CraftingLog = Log;
-    //     this.isUpadateMode = true;
-    //   }
   }
 };
 </script>
+
+// const ZeroString = "0000000"; // const ZeroPadding =
+`${ZeroString}${ID}`.slice(7); // const DocumentID = `Log${ZeroPadding}`; //
+console.log(DocumentID);
 
 <style lang="scss">
 .LogRegister {
   display: flex;
   align-items: center;
-  flex-direction: column-reverse;
+  flex-direction: column;
 
   @media screen and (min-width: 780px) {
-    flex-direction: row;
+    flex-direction: column;
+  }
+
+  &__Body {
+    margin: 5px;
   }
 
   &__Ingredients {
