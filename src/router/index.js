473fbcac854ac4d6240442d74447effc07a69ac2
OKAUEND <def1f23sbb@gmail.com>
2020-03-23 19:06:46 +0900

    ADD - 判定にfirebaseにログインしてるかを参照するように

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import firebase from "@/firebase.js";
 
 import Singin from "@/components/Console/singin.vue";
 import Console from "@/components/Console/Console.vue";
@@ -40,7 +41,7 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(recode => recode.meta.requiresAuth);
-  if (requiresAuth) {
+  if (requiresAuth && !firebase.getCurrentUser()) {
     next({ path: "/", query: { redirect: to.fullPath } });
   } else {
     next();
