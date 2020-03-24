9b8591d9439299df63f575384a20e457df4d31a6
OKAUEND <def1f23sbb@gmail.com>
2020-03-24 14:05:09 +0900

    ADD - ページタイトルを設定

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,11 @@ module.exports = {
         prependData: '@import "./src/assets/scss/prepends.scss";'
       }
     }
+  },
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "FFXIV CraftTools 管理画面"
+    }
   }
 };
