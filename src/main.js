6c228417aa522351b18dc8db1c6766195da9f5d6
OKAUEND <def1f23sbb@gmail.com>
2020-04-15 08:59:05 +0900

    Chore - 取得したデータをキャッシュしたいため、Vuexを追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Vuex from "vuex";
 require("@/assets/scss/main.scss");
 
+Vue.use(Vuex);
 Vue.config.productionTip = false;
 
 new Vue({
