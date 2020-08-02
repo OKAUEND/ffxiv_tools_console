7f87711693b75fad442c36fd4d68c85fb8c4f7a1
OKAUEND <def1f23sbb@gmail.com>
2020-08-03 03:30:46 +0900

    New - データを取得する責務を分離させるために親切

---
+++ src/components/Console/StoreList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="writeInterface()">追加/更新</button>
+    調整したいIL帯を選択してください
+    <input type="text" placeholder="上限IL" />
+    <input type="text" placeholder="下限IL" />
+    <button @click="onFetchStoreLogs()">取得</button>
+    <ul>
+      <li v-for="(Log, ID) in Logs" :key="ID">
+        <button @click="bindLog(Log)">{{ Log.Name }}</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StoreList",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
