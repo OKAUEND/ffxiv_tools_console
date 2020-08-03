af424c82f537b0ece5ee189fd2fa6740e5da6376
OKAUEND <def1f23sbb@gmail.com>
2020-08-03 09:01:42 +0900

    ADD - 元のコンポーネントで使われていた処理を移植 移植時にコメントの追加と、変数名がわかりにくかったのでより適したものに変更した

--- src/components/Console/StoreList.vue
+++ src/components/Console/StoreList.vue
@@ -6,18 +6,52 @@
     <input type="text" placeholder="下限IL" />
     <button @click="onFetchStoreLogs()">取得</button>
     <ul>
-      <li v-for="(Log, ID) in Logs" :key="ID">
-        <button @click="bindLog(Log)">{{ Log.Name }}</button>
+      <li v-for="(Log, ID) in StoreLogs" :key="ID">
+        <button @click="selectProps(Log)">{{ Log.Name }}</button>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import firebase from "@/firebase.js";
 export default {
   name: "StoreList",
   data() {
-    return {};
+    return {
+      StoreLogs: []
+    };
+  },
+  methods: {
+    onFetchStoreLogs() {
+      //選択したレベル帯に該当するログを取得する
+      this.fetchCraftLogs().then(Logs => {
+        console.log(Logs);
+        this.StoreLogs = Logs;
+      });
+    },
+
+    async fetchCraftLogs() {
+      //Firestoreからデータを取り出すクエリを作成する
+      const docRef = firebase
+        .firestore()
+        .collection("CraftLog")
+        .orderBy("ID");
+
+      //選択したレベル帯に該当するログを取得する
+      return await docRef
+        .get()
+        .then(querySnapshot => {
+          const docs = querySnapshot.docs.map(doc => doc.data());
+          return docs;
+        })
+        .catch(error => {
+          console.error("Firest Error getting document", error);
+        });
+    },
+    selectProps(Data) {
+      this.$props("click", Data);
+    }
   }
 };
 </script>
