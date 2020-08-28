8c4be037371bbf2bd791b5109b727b89f55cd499
OKAUEND <def1f23sbb@gmail.com>
2020-08-28 11:30:25 +0900

    Change - フォルダ階層を移動

---
+++ src/components/Console/CategoryList.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <slot />:
+    <li v-for="(value, key) in Categories" :key="key">
+      <input type="radio" :id="value.type" :value="value.type" v-model="type" />
+      <label :for="value">{{ value.name }}</label>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "CategoryList",
+  props: {
+    Category: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    Categories() {
+      return this.Category;
+    }
+  }
+};
+</script>
+
+<style></style>
