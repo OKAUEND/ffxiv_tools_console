d288ee04870f445b69bfbce03c83fba055555d80
OKAUEND <def1f23sbb@gmail.com>
2020-08-30 23:28:04 +0900

    Add - 全データ取得だと容量と読み出し回数に無駄が多いのでクエリの条件を加える

--- src/components/Console/StoreList.vue
+++ src/components/Console/StoreList.vue
@@ -33,13 +33,14 @@ export default {
 
     async fetchCraftLogs() {
       //Firestoreからデータを取り出すクエリを作成する
-      const docRef = firebase
-        .firestore()
-        .collection("CraftLog")
-        .orderBy("ID");
-
-      //選択したレベル帯に該当するログを取得する
-      return await docRef
+      const docRef = firebase.firestore().collectionGroup("CraftLog");
+      //すべてを取得するのは負荷がかかるため、選択したクラフターと同じもので、IL帯で絞り込みを行えるようにする
+      const queryRef = docRef
+        .where("job", "==", this.job)
+        .where("itemlevel", "<=", this.upperItemlevel)
+        .where("itemlevel", ">=", this.lowerItemlevel);
+      //DBへ問い合わせる
+      return await queryRef
         .get()
         .then(querySnapshot => {
           const docs = querySnapshot.docs.map(doc => doc.data());
