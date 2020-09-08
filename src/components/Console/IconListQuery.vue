24318e4aa81257eb11d4e5a112f239a469e2577c
OKAUEND <def1f23sbb@gmail.com>
2020-09-08 22:28:37 +0900

    New - 検索したいデータを選択欄形式で選べるような表示用のコンポーネントを作成

---
+++ src/components/Console/IconListQuery.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <category-list :Category="iconcategory.crafter" v-model="crafter">
+      クラフター</category-list
+    >
+    <category-list :Category="category.rank" v-model="rank">段階</category-list>
+    <category-list :Category="iconcategory.groups" v-model="group"
+      >種類</category-list
+    >
+    <category-list :Category="icondetails" v-model="detail">詳細</category-list>
+  </div>
+</template>
+
+<script>
+import CategoryList from "@/components/Console/BaseRadioButtonList.vue";
+import divisions from "../../assets/category.json";
+import iconobject from "../../assets/categoryicons.json";
+export default {
+  name: "IconListQuery",
+  components: {
+    CategoryList
+  },
+  data() {
+    return {
+      job: "",
+      crafter: "",
+      rank: "",
+      group: "",
+      detail: ""
+    };
+  },
+  computed: {
+    category() {
+      return divisions;
+    },
+    iconcategory() {
+      return iconobject;
+    },
+    icondetails() {
+      switch (this.group) {
+        case "Material":
+          return this.iconcategory.material;
+        case "Weapon":
+          return this.iconcategory.battlejob;
+        default:
+          return [];
+      }
+    },
+    emit() {
+      return this.$emit("change");
+    }
+  }
+};
+</script>
+
+<style></style>
