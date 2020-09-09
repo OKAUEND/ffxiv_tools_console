f725dd4ed7c17166c49761cbd33b223df0b30d51
OKAUEND <def1f23sbb@gmail.com>
2020-09-09 17:02:42 +0900

    Add - firestoreよりアイコン画像の一覧を取得するメソッドを追加

--- src/components/Console/IconList.vue
+++ src/components/Console/IconList.vue
@@ -4,7 +4,40 @@
 </template>
 
 <script>
+import firebase from "@/firebase.js";
 export default {
   name: "IconList",
+  },
+  data() {
+    return {
+      icons: [],
+    };
+  },
+  methods: {
+    /**
+     * Firestoreから条件にあったアイコンの一覧を取得する
+     */
+    async fetchIconDetailes(value) {
+      //選択した内容から、Firestoreへ通信を行うdocumentを作成する
+      const documentRef = firebase
+        .firestore()
+        .collection("Image")
+        .doc(value.document)
+        .collection(value.collection)
+        .where("Type", "==", value.typecriteria)
+        .where("MaterialType", "==", value.materialtypecriteria);
+
+      //0から再描画させたいのでIconsを初期化する
+      this.icons.length = 0;
+
+      const querysnapshot = documentRef.get();
+
+      this.icons = await querysnapshot.then(querySnapshot => {
+        return querySnapshot.docs.map(doc => {
+          return doc.data();
+        });
+      });
+    }
+  }
 };
 </script>
