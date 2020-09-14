1fd1dbf2110b12b88e6d316a070098ed3c84adb9
OKAUEND <def1f23sbb@gmail.com>
2020-09-14 23:36:28 +0900

    New - 2つの配列を1つにマージするための外部ファイルを作成 ・ただマージするだけではなく、同じデータは省きつつマージしたいのため自らコーディングする ・またこのファイル自体を別プロジェクトで使いたいため、外部ファイルにしておく

---
+++ src/listmergeutile.js
@@ -0,0 +1,14 @@
+export default {
+  /**
+   * 配列をマージし一つの配列にする
+   * @param {Array} parent - マージ先の配列
+   * @param {Array} target - マージしたい配列
+   */
+  mergeList(parent, target) {},
+  /**
+   * 同じ状態のデータを除外した配列を生成する
+   * @param {Array} parent - 重複データを検索する配列
+   * @param {Array} target - 重複データを除外する配列
+   */
+  filtersamedata(parent, target) {}
+};
