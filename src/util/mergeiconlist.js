13b487661088e8c5d98fc23dc5b9fad59510b916
OKAUEND <def1f23sbb@gmail.com>
2020-09-15 20:00:27 +0900

    Change - 他にも外部ファイルを実装する可能性があるため、それ用のフォルダを作成 また、ファイル名も変更

---
+++ src/util/mergeiconlist.js
@@ -0,0 +1,16 @@
+export default mergeiconlist;
+/**
+ * 配列をマージし一つの配列にする
+ * @param {Array} parent - マージ先の配列
+ * @param {Array} target - マージしたい配列
+ */
+function mergeiconlist(parent, target) {
+}
+
+/**
+ * 同じ状態のデータを除外した配列を生成する
+ * @param {Array} parent - 重複データを検索する配列
+ * @param {Array} target - 重複データを除外する配列
+ */
+function filtersamedata(parent, target) {
+}
