1c6a08b9716f44e8a035070e95fc10b17aadfe97
OKAUEND <def1f23sbb@gmail.com>
2020-09-16 01:27:26 +0900

    Add - Vuexの設定

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Vuex from "vuex";
+import store from "./store";
 require("@/assets/scss/main.scss");
 
 Vue.use(Vuex);
@@ -9,5 +10,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import icon from "./modules/icon.js";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({});
+export default new Vuex.Store({
+  modules: {
+    icon
+  }
+});
