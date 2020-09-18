f3ac38302ed631a9d454115ab02fd302cd665ccd
OKAUEND <def1f23sbb@gmail.com>
2020-09-18 22:24:08 +0900

    Add - 取得した配列をキャッシュしたいので、配列を加えたオブジェクトで同じ内容のを更新をかける

--- src/util/mergeiconlist.js
+++ src/util/mergeiconlist.js
@@ -24,4 +24,24 @@ function filtersamedata(parent, target) {
     return !parent.includes(item.ID) && !parent.includes(item.MaterialType);
   });
 }
+
+/**
+ * 更新を行うために必要な配列をもったObjectを探す
+ * @param {Array} parent - 重複データを検索する配列
+ * @param {Array} target - 重複データを除外する配列
+ */
+function updateIconArray(targets, commit) {
+  //更新するアイテムのインデックスを探す
+  const targetindex = targets.findIndex(target => {
+    {
+      return (
+        target.document === commit.document &&
+        target.collection === commit.collection &&
+        target.crafter === commit.crafter &&
+        target.detail === commit.detail
+      );
+    }
+  });
+
+  return targets.splice(targetindex, 1, commit);
 }
