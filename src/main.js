64c43c8eb42452a4d5638c843b1889cf7b759be4
OKAUEND <def1f23sbb@gmail.com>
2020-09-20 23:23:21 +0900

    Remove - 不要なVuexの読み込み設定を削除

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import Vuex from "vuex";
 import store from "./store";
 require("@/assets/scss/main.scss");
 
-Vue.use(Vuex);
 Vue.config.productionTip = false;
 
 new Vue({
