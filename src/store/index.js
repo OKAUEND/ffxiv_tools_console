c628bd0a3a1409ddbbfa91ef1c5f3ee5dbd975d8
OKAUEND <def1f23sbb@gmail.com>
2020-09-25 11:22:40 +0900

    New - 製作レシピを取得した際に、一時キャッシュを行えるようにする ・storeにモジュールを追加し、準備

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import icon from "./modules/icon.js";
+import log from "./modules/log.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    icon
+    icon,
+    log
   }
 });
